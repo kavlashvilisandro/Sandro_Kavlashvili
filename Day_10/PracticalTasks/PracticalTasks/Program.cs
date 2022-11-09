1a4a5bb68fea4e682ec79a211476af108b0b9353
SandroKavlash <kavlashvilisandro1@gmail.com>
2022-11-09 17:50:24 +0400

    Homework Finished

---
+++ Day_10/PracticalTasks/PracticalTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PracticalTasks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat catInstance = new Cat();
+            Console.Write("Enter name: ");
+            catInstance.Name = Console.ReadLine();
+            Console.Write("Enter breed: ");
+            catInstance.Breed = Console.ReadLine();
+            Console.Write("Enter age: ");
+            catInstance.Age = int.Parse(Console.ReadLine());
+            Console.Write("Enter sex: ");
+            catInstance.Sex = Console.ReadLine();
+            Console.WriteLine("Cat Object Created");
+            Console.WriteLine($"Name: {catInstance.Name} | Breed: {catInstance.Breed} | Age: {catInstance.Age} | Sex: {catInstance.Sex}");
+            Console.Write("Enter food wight in grams: ");
+            catInstance.Eat(int.Parse(Console.ReadLine()));
+            Console.Write("Enter meowing amount: ");
+            catInstance.Meow(int.Parse(Console.ReadLine()));
+        }
+        
+    }
+}
---
+++ Day_10/PracticalTasks/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangleInstance = new Triangle();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter length of side {i + 1}: ");
+                triangleInstance[i] = int.Parse(Console.ReadLine());
+            }
+            triangleInstance.PrintSides();
+            Console.WriteLine($"Perimeter of the triangle is: {triangleInstance.getPerimeter()}");
+            Console.WriteLine($"Area of triangle is {triangleInstance.getArea()}");
+        }
+    }
+}
