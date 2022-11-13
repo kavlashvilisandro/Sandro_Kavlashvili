a4d970da818b70325878c27162f110e168a92a43
SandroKavlash <kavlashvilisandro1@gmail.com>
2022-11-13 22:49:11 +0400

    Homework Finished

---
+++ Day_12/PracticalTasks/PracticalTasks/MATH.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticalTasks
+{
+    public static class MATH
+    {
+        private enum Statuses
+        {
+            PowMustBeaPositiveOrZero,
+            Success,
+            MaxReturned,
+            MinReturned,
+            TheyAreEqual,
+        }
+
+        /*
+         This method returns -1 if power is less then zero
+         */
+        public static double Power(double number, int power, out string status)
+        {
+            if(power < 0)
+            {
+                status = Statuses.PowMustBeaPositiveOrZero.ToString();
+                return -1;
+            }
+            status = Statuses.Success.ToString();
+            if(power == 0)
+            {
+                return 1;
+            }
+            return number * Power(number, power - 1, out status);
+        }
+        public static double Min(double number1, double number2, out string status)
+        {
+            if(number1 == number2)
+            {
+                status = Statuses.TheyAreEqual.ToString();
+                return number1;
+            }else if(number1 > number2)
+            {
+                status = Statuses.MinReturned.ToString();
+                return number2;
+            }
+            else
+            {
+                status = Statuses.MinReturned.ToString();
+                return number1;
+            }
+        }
+
+        public static double Max(double number1, double number2,out string status)
+        {
+            if (number1 == number2)
+            {
+                status = Statuses.TheyAreEqual.ToString();
+                return number1;
+            }
+            else if (number1 > number2)
+            {
+                status = Statuses.MaxReturned.ToString();
+                return number1;
+            }
+            else
+            {
+                status = Statuses.MaxReturned.ToString();
+                return number2;
+            }
+        }
+    }
+}
---
+++ Day_12/PracticalTasks/PracticalTasks/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace PracticalTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string status = string.Empty;
+            double number;
+            int power;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("TASK1 | to navigate to the next task, write \"yes\" is last input");
+            Console.ForegroundColor= ConsoleColor.White;
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                number = double.Parse(Console.ReadLine());
+                Console.Write("Enter Power: ");
+                power= int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{number} to the power of {power} is: {MATH.Power(number,power,out status)}");
+                Console.WriteLine($"Status: {status}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("continue with next task(yes|no): ");
+                if(Console.ReadLine() == "yes")
+                {
+                    break;
+                }
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("TASK2 | to navigate to the next task, write \"yes\" is last input");
+            Console.ForegroundColor = ConsoleColor.White;
+            double number1,number2;
+            while (true)
+            {
+                Console.Write("Enter number1: ");
+                number1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter number2: ");
+                number2 = double.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Smaller is {MATH.Min(number1,number2,out status)}");
+                Console.WriteLine("Status: " + status);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("continue with next task(yes|no): ");
+                if (Console.ReadLine() == "yes")
+                {
+                    break;
+                }
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("TASK3 | to navigate to the next task, write \"yes\" is last input");
+            Console.ForegroundColor = ConsoleColor.White;
+            while (true)
+            {
+                Console.Write("Enter number1: ");
+                number1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter number2: ");
+                number2 = double.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Bigger is {MATH.Max(number1, number2, out status)}");
+                Console.WriteLine("Status: " + status);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("continue with next task(yes|no): ");
+                if (Console.ReadLine() == "yes")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
