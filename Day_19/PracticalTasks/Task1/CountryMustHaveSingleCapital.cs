87694865c5b0e2a49e0464ea4edcf462bcd32bed
SandroKavlash <kavlashvilisandro1@gmail.com>
2022-11-30 01:22:59 +0400

    Home work finished

---
+++ Day_19/PracticalTasks/Task1/CountryMustHaveSingleCapital.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    internal class CountryMustHaveSingleCapital : Exception
+    {
+        public CountryMustHaveSingleCapital() : base("CountryMustHaveSingleCapital")
+        {
+
+        }
+    }
+}
---
+++ Day_19/PracticalTasks/Task1/GeographicEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public abstract class GeographicEntity
+    {
+        public abstract int GetPopulation();
+        public abstract double GetTotalArea();
+    }
+}
