b8539f5652ca81649c5724b6264278b2eef1f395
SandroKavlash <kavlashvilisandro1@gmail.com>
2023-01-11 16:21:04 +0400

    HomeWork finished

---
+++ Day_34/PracticalTask/PracticalTask/MiddleWares/UnHandledExceptionHandlerMiddleWare.cs
@@ -0,0 +1,36 @@
+﻿namespace PracticalTask.MiddleWares
+{
+    public class UnHandledExceptionHandlerMiddleWare
+    {
+public static Dictionary<Exception, DateTime> exceptions = new Dictionary<Exception , DateTime>();
+        private readonly RequestDelegate _next;
+        public UnHandledExceptionHandlerMiddleWare(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _next.Invoke(context);
+            await CheckErrors();
+        }
+
+        public async Task CheckErrors()
+        {
+            for(int i = 0; i < exceptions.Count; i++)
+            {
+                await LogExceptions(i);
+            }
+        }
+
+        public async Task LogExceptions(int index)
+        {
+            File.AppendAllText("Errors.txt", $"error Message: " +
+                $"{exceptions.ElementAt(index).Key.Message}\n" +
+                $"Time: {exceptions[exceptions.ElementAt(index).Key].ToString()}\n");
+            exceptions.Remove(exceptions.ElementAt(index).Key);
+        }
+
+        
+    }
+}
